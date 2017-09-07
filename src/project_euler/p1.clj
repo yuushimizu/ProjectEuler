ecdb9fb60f30166925c318ef2cd6f186bea9a34f
Yuu Shimizu <p@yuushimizu.com>
2017-09-07 21:38:50 +0900

    reduce code

--- src/project_euler/p1.clj
+++ src/project_euler/p1.clj
@@ -1,8 +1,6 @@
-(ns project-euler.p1
-  (:require [project-euler.util :refer :all]))
+(require '[project-euler.util :refer :all])
 
-(println
- (->> (range)
-      (filter #(or (div? % 3) (div? % 5)))
-      (take-while #(< % 1000))
-      (sum)))
+(->> (range)
+     (filter #(or (div? % 3) (div? % 5)))
+     (take-while #(< % 1000))
+     (sum))
