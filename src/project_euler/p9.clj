644973237698c414600a81a6b857e3b25b33e92d
Yuu Shimizu <p@yuushimizu.com>
2017-09-10 03:27:40 +0900

    solve 9

---
+++ src/project_euler/p9.clj
@@ -0,0 +1,12 @@
+(require '[project-euler.util :refer :all]
+         '[project-euler.util.predicates :as p])
+
+(defn pythagorean-triplet? [a b c]
+  (= (+ (square a) (square b)) (square c)))
+
+(->> (range)
+     (mapcat (fn [a] (->> (range (inc a) (/ (- 1000 a) 2))
+                          (map #(vector a % (- 1000 a %))))))
+     (filter #(apply pythagorean-triplet? %))
+     (first)
+     (reduce *))
