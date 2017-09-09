248728528941e2de318b661306246200d70f8597
Yuu Shimizu <p@yuushimizu.com>
2017-09-10 02:30:38 +0900

    reduce the code of problem 3

--- src/project_euler/p3.clj
+++ src/project_euler/p3.clj
@@ -2,13 +2,8 @@
          '[project-euler.util.predicates :as p])
 
 (->> (cons 2 (range-from 3 2))
-     (reductions (fn [{n :n} candidate]
-                   (let [factor? (div? n candidate)]
-                     {:candidate candidate
-                      :n (if factor? (/ n candidate) n)
-                      :factor? factor?}))
-                 {:candidate 1 :n 600851475143 :factor? false})
-     (filter (fn [{factor? :factor?}] factor?))
-     (take-while (fn [{n :n}] (> n 1)))
-     (last)
-     :n)
+     (reductions (fn [[_ n] candidate] [candidate (if (div? n candidate) (/ n candidate) n)])
+                 [1 600851475143])
+     (filter (fn [[_ n]] (= n 1)))
+     (first)
+     (first))
