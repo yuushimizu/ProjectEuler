ff40431e58fa301744406d453a6dc6af25fbde01
Yuu Shimizu <p@yuushimizu.com>
2017-09-14 22:12:29 +0900

    add fast 1

--- src/project_euler/p1.clj
+++ src/project_euler/p1.clj
@@ -2,8 +2,16 @@
   (:require [project-euler.util :refer :all]
             [project-euler.util.predicates :as p]))
 
+; 5 ms
+;(defn solve []
+;  (->> (range 1000)
+;       (filter (p/or (p/div? 3) (p/div? 5)))
+;       (sum)))
+
+; 0.15ms
 (defn solve []
-  (->> (range)
-       (filter (p/or (p/div? 3) (p/div? 5)))
-       (take-while (p/< 1000))
-       (sum)))
+  (let [limit 1000]
+    (letfn [(sum-of-div [n]
+              (let [nums (quot (dec limit) n)]
+                (quot (* n nums (inc nums)) 2)))]
+      (- (+ (sum-of-div 3) (sum-of-div 5)) (sum-of-div 15)))))
